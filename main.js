a799922ffdd41e7ccc7b3050f536c5a0d2076d90
Marcelo de Araújo Dantas <136765367+MahawDantas@users.noreply.github.com>
2023-09-14 23:55:34 -0300

    correção-2

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const form = document.getElementById("form-comparador");
 form.addEventListener("submit", function(evento) {
     evento.preventDefault();
 
-    const numeroA = document.getElementById("numero-a").value;
-    const numeroB = document.getElementById("numero-b").value;
+    const numeroA = parseFloat(document.getElementById("numero-a").value);
+    const numeroB = parseFloat(document.getElementById("numero-b").value);
     const mensagemSucesso = `O número: <b>${numeroB}</b> é maior que o número: <b>${numeroA}</b>`;
     const mensagemError = `O número: <b>${numeroB}</b> é menor que o número: <b>${numeroA}</b>`;
 
@@ -12,7 +12,6 @@ form.addEventListener("submit", function(evento) {
         const containerMensagemError = document.querySelector(".error-message");
         containerMensagemError.innerHTML = mensagemError;
         containerMensagemError.style.display = "block";
-        containerMensagemSucesso.style.display = "none";
 
         document.getElementById("numero-a").value = "";
         document.getElementById("numero-b").value = "";
@@ -22,8 +21,7 @@ form.addEventListener("submit", function(evento) {
         const containerMensagemSucesso = document.querySelector(".success-message");
         containerMensagemSucesso.innerHTML = mensagemSucesso;
         containerMensagemSucesso.style.display = "block";
-        containerMensagemError.style.display = "none";
 
         document.getElementById("numero-a").value = "";
         document.getElementById("numero-b").value = "";
-}})
+}});
